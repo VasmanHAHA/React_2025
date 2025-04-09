ace7ae2f5e843272cd00a4980e1177633bd70f12
Vasiliy Znaida <znvs1594@gmail.com>
2025-04-09 14:10:24 +0300

    first store

--- src/pages/redux/Redux.page.tsx
+++ src/pages/redux/Redux.page.tsx
@@ -1,10 +1,38 @@
 import { Header } from "@/shared/widgets/header/header";
+import { Button } from "@mantine/core";
+import classes from './classes.redux.page.module.css'
+import { DecrementAction, IncrementAction, store } from "@/shared/store/store";
+import { useEffect, useSyncExternalStore } from "react";
 
 export function ReduxPage() {
-  return (<>
-    <Header />
-    <p>redux practice will be here</p>
+  // const count = store.getState().counter;
 
-  </>
+  // useEffect(()=> {
+  //   console.log(count);
+  // }, [count])
+
+  const count = useSyncExternalStore(
+    (onStoreChange) => store.subscribe(onStoreChange),
+    () => store.getState().counter,
+  );
+
+  return (
+    <>
+      <Header />
+      <section className={classes.counterSection}>
+        <Button onClick={() => {
+          store.dispatch({ type: "increment" } satisfies IncrementAction)
+        }
+        }> Increment</Button>
+        <Button onClick={() => {
+          store.dispatch({ type: "decrement" } satisfies DecrementAction)
+        }
+        }
+
+        > Decrement</Button>
+        <div> counter: {count}</div>
+      </section>
+
+    </>
   );
 }
